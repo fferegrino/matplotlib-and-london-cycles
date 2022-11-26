35b34380fd446abd03eecf4cadfc03212e4e8635
Antonio Feregrino <antonio.feregrino@gmail.com>
2022-11-26 19:54:13 +0000

    Stilize scatterplot

--- plot.py
+++ plot.py
@@ -23,6 +23,34 @@ def plot_station_usage(data, fig, ax):
     london_map.plot(ax=ax, color="#F4F6F7", edgecolor="black", linewidth=0.5)
 
     # Main drawing method
+    sns.scatterplot(
+        y="lat",
+        x="lon",
+        hue="occupancy",
+        data=data,
+        legend=False,
+        palette="OrRd",
+        s=25,
+        edgecolor="k",
+        linewidth=0.1,
+        ax=ax,
+    )
+
+    # Set limits (+padding) and remove axis
+    lats = data["lat"].min() - 0.005, data["lat"].max() + 0.005
+    lons = data["lon"].min() - 0.02, data["lon"].max() + 0.02
+    ax.set_ylim(lats[0], lats[1])
+    ax.set_xlim(lons[0], lons[1])
+    ax.set_axis_off()
+
+
+def plot_station_usage_v4(data, fig, ax):
+
+    # Draw map
+    london_map = gpd.read_file("london-cycles-db/shapefiles/London_Borough_Excluding_MHW.shp").to_crs(epsg=4326)
+    london_map.plot(ax=ax, color="#F4F6F7", edgecolor="black", linewidth=0.5)
+
+    # Main drawing method
     sns.scatterplot(y="lat", x="lon", hue="occupancy", data=data, legend=False, ax=ax)
 
     # Set limits (+padding) and remove axis
