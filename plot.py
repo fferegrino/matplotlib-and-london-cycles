d06010a4b46c3fb4d8a50bd6be43b3c4050a39e9
Antonio Feregrino <antonio.feregrino@gmail.com>
2022-11-26 19:48:40 +0000

    Color map

--- plot.py
+++ plot.py
@@ -20,6 +20,23 @@ def plot_station_usage(data, fig, ax):
 
     # Draw map
     london_map = gpd.read_file("london-cycles-db/shapefiles/London_Borough_Excluding_MHW.shp").to_crs(epsg=4326)
+    london_map.plot(ax=ax, color="#F4F6F7", edgecolor="black", linewidth=0.5)
+
+    # Main drawing method
+    sns.scatterplot(y="lat", x="lon", hue="occupancy", data=data, legend=False, ax=ax)
+
+    # Set limits (+padding) and remove axis
+    lats = data["lat"].min() - 0.005, data["lat"].max() + 0.005
+    lons = data["lon"].min() - 0.02, data["lon"].max() + 0.02
+    ax.set_ylim(lats[0], lats[1])
+    ax.set_xlim(lons[0], lons[1])
+    ax.set_axis_off()
+
+
+def plot_station_usage_v3(data, fig, ax):
+
+    # Draw map
+    london_map = gpd.read_file("london-cycles-db/shapefiles/London_Borough_Excluding_MHW.shp").to_crs(epsg=4326)
     london_map.plot(ax=ax)
 
     # Main drawing method
